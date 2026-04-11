d01d6089cab9a4c5549ac50829db3d69300952f4
alexkm13 <alexkim9133@gmail.com>
2026-04-10 21:37:41 -0400

    Added division and fixed typing issues

--- src/field.rs
+++ src/field.rs
@@ -1,8 +1,9 @@
 // galois field (GF^8) operations
 
 const W: u16 = 8; // w in 2^w -- hardcode to 8 to represent bytes
-const LOG_ARR: [u8; 256] // log table for quick access
-const EXP_ARR: [u8; 512] // exponential table for inverse log table
+const LOG_ARR: [u8; 256]; // log table for quick access
+const EXP_ARR: [u8; 512]; // exponential table for inverse log table
+const NW_MINUS_1: u16 = 255;
 
 fn add(a: u8, b: u8) -> u8 {
     (a ^ b)
@@ -12,16 +13,36 @@ fn subtract(a: u8, b: u8) -> u8 {
     (a ^ b)
 }
 
-fn mult(a: u8, b: u8) -> usize {
+fn mult(a: u8, b: u8) -> u8 {
     let mut sum_log: u16;
     if a == 0 || b == 0 {
-        0
-    };
-
-    sum_log = LOG_ARR[a as usize] + LOG_ARR[b as usize];
-    if sum_log >= 2u16.pow(W) - 1 {
-        sum_log -= 2u16.pow(W) - 1;
+        return 0;
     }
 
-    EXP_ARR[sum_log as usize]
+    sum_log = LOG_ARR[a as usize] as u16 + LOG_ARR[b as usize] as u16;
+    if sum_log >= NW_MINUS_1 {
+        sum_log -= NW_MINUS_1;
+    }
+
+    EXP_ARR[sum_log as usize] 
+}
+
+fn div(a: u8, b: u8) -> u8 {
+    let mut diff_log: i16;
+    
+    if a == 0 {
+        return 0;
+    }
+    
+    match b {
+        0 => panic!("Cannot divide by zero."),
+        _ => {},
+    }
+
+    diff_log = LOG_ARR[a as usize] as i16 - LOG_ARR[b as usize] as i16;
+    if diff_log < 0 {
+        diff_log += (NW_MINUS_1) as i16;
+    }
+
+    EXP_ARR[diff_log as usize] 
 }
